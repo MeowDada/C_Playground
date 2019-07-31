26f0f318228637907f0a398a8a9e6d25e606270d
MeowDada <chang.hungjui1251@gmail.com>
2019-07-31 11:54:02 +0800

    Add double_pointer_ops.c

---
+++ Pointers/double_pointer_ops.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+void try_assign_double_pointer(int **double_ptr)
+{
+    LOGGING_INFO("Try assign *double_ptr = 5");
+    if (*double_ptr) {
+        *double_ptr = 5;
+    }
+    else {
+        LOGGING_INFO("*double_ptr is NULL, cannot assign value to it");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    setup_logger(NULL, LOG_LEVEL_INFO);
+
+    int value = 5;
+    int **double_ptr = NULL;
+    LOGGING_INFO("Initialize int value = %d, int **double_ptr = NULL", value);
+
+    try_assign_double_pointer(double_ptr);
+
+    if (*double_ptr) {
+        LOGGING_INFO("*double_ptr = %d", *double_ptr);
+    }
+    else {
+        LOGGING_INFO("Cannot dereference *double_ptr, because it is a NULL pointer");
+    }
+    
+    return 0;
+}
