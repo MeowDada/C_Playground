e71f8492878b7ac4bc1fcfb2bfce6ec181d4521b
MeowDada <chang.hungjui1251@gmail.com>
2019-07-31 12:02:39 +0800

    Fix RE

--- Pointers/double_pointer_ops.c
+++ Pointers/double_pointer_ops.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include "common.h"
 
-void try_assign_double_pointer(int *value, int **double_ptr)
+void try_assign_double_pointer(int *single_ptr, int **double_ptr)
 {
     LOGGING_INFO("Try assign *double_ptr = value");
-    *double_ptr = value;
+    *double_ptr = single_ptr;
 }
 
 int main(int argc, char **argv)
 {
     setup_logger(NULL, LOG_LEVEL_INFO);
 
-    int value = 5;
+    int   value      = 5;
+    int  *single_ptr = &value;
     int **double_ptr = NULL;
     LOGGING_INFO("Initialize int value = %d, int **double_ptr = NULL", value);
 
-    try_assign_double_pointer(&value, double_ptr);
+    try_assign_double_pointer(single_ptr, double_ptr);
 
     if (*double_ptr) {
         LOGGING_INFO("&value       = %p", &value);
