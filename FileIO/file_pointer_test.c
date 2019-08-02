ea94efc0d960ef2e2eaf30b6b85dc08da5a98024
MeowDada <chang.hungjui1251@gmail.com>
2019-08-02 12:23:47 +0800

    TEST

--- FileIO/file_pointer_test.c
+++ FileIO/file_pointer_test.c
@@ -6,8 +6,11 @@
 #include <sys/stat.h>
 #endif
 
-static void print_buffer_content(char *buffer)
+static void print_buffer_content(char *buffer, int end)
 {
+    if (end < 0)
+        end = 0;
+    buffer[end] = '\0';
     LOGGING_INFO("buffer_content = %s", buffer);
 }
 
@@ -128,9 +131,9 @@ int main(int argc, char **argv)
 
 
     retval = fread(buffer, 16, 1, fp);
-    LOGGING_INFO("[#%3d] fread(buffer, 16, 1, fp) = %d", steps++, retval);
+    LOGGING_INFO("[#%3d] fread(buffer, 1, 16, fp) = %d", steps++, retval);
     print_all_info(fp);
-    print_buffer_content(buffer);
+    print_buffer_content(buffer, 16);
 
 end:
     close_logger(NULL);
