d55cc85e084cde3c9a1804a2ca845b8e46676de2
MeowDada <chang.hungjui1251@gmail.com>
2019-08-19 12:04:23 +0800

    Add socket

---
+++ Linux/Socket/tcp_client.c
@@ -0,0 +1,70 @@
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h>
+#include "logger.h"
+
+#define MAX 80 
+#define PORT 8080
+
+static void do_stuff(int sockfd)
+{
+    char buf[MAX];
+    int n = 0;
+    while (1) {
+        bzero(buf, sizeof(buf));
+        LOGGING_INFO("Enter the string : ");
+        n = 0;
+        while ((buf[n++] = getchar()) != '\n');
+
+        write(sockfd, buf, sizeof(buf));
+        bzero(buff, sizeof(buf));
+        read)(sockfd, buf, sizeof(buf));
+        LOGGING_INFO("from server : %s", buf);
+        if ((strncmp(buf, "exit", 4)) == 0) {
+            LOGGING_INFO("client exit...");
+            break;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    setup_logger(NULL, LOG_LEVEL_INFO);
+
+    int sockfd, connfd, len;
+
+    struct sockaddr_in serv_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        LOGGING_ERROR("socket creation failed...");
+        goto end;
+    }
+    else {
+        LOGGING_INFO("socket created successfully");
+    }
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    /* Assign IP and ports */
+    serv_addr.sin_family      = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl("127.0.0.1");
+    serv_addr.sin_port        = htons(PORT);
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+        LOGGING_ERROR("connection with the server failed...");
+        goto end;
+    }
+    else {
+        LOGGING_INFO("connected to the server");
+    }
+
+    do_stuff(connfd);
+
+    close(sockfd);
+end:
+    close_logger(NULL);
+
+    return 0;
+}
---
+++ Linux/Socket/tcp_server.c
@@ -0,0 +1,95 @@
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include "logger.h"
+
+#define MAX 80 
+#define PORT 8080 
+
+static void do_stuff(int sockfd)
+{
+    char buf[MAX];
+    int n = 0;
+
+    while (1) {
+        bzero(buf, MAX);
+
+        read(sockfd, buff, sizeof(buf));
+        LOGGING_INFO("from client: %s\t to client : ", buf);
+        bzero(buf, MAX);
+        n = 0;
+
+        while((buf[n++] = getchar()) != '\n');
+
+        write(sockfd, buff, sizeof(buf));
+
+        if (strncmp("exit", buf, 4) == 0) {
+            LOGGING_INFO("server exit...");
+            break;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    setup_logger(NULL, LOG_LEVEL_INFO);
+
+    int sockfd, connfd, len;
+
+    struct sockaddr_in serv_addr, cli;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        LOGGING_ERROR("socket creation failed...");
+        goto end;
+    }
+    else {
+        LOGGING_INFO("socket created successfully");
+    }
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    /* Assign IP and ports */
+    serv_addr.sin_family      = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port        = htons(PORT);
+
+    if ((bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) != 0) {
+        LOGGING_ERROR("socket bind failed...");
+        goto end;
+    }
+    else {
+        LOGGING_INFO("socket bind successfully");
+    }
+
+    /* start listening */
+    if ((listen(sockfd, 5)) != 0) {
+        LOGGING_ERROR("listen failed...");
+        goto end;
+    }
+    else {
+        LOGGING_INFO("server listening...");
+    }
+
+    len = sizeof(cli);
+
+    /* Accept the data packet from client and verification */
+    connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+    if (connfd < 0) {
+        LOGGING_ERROR("server accept failed...");
+        goto end;
+    }
+    else {
+        LOGGING_INFO("server accept the client...");
+    }
+    do_stuff(connfd);
+
+    close(sockfd);
+
+end:
+    close_logger(NULL);
+
+    return 0;
+}
