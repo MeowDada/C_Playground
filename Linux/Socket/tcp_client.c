bbc41a9bdab8bd31caf93117c42bf21a13a38616
MeowDada <chang.hungjui1251@gmail.com>
2019-08-21 10:47:16 +0800

    Fix CE

--- Linux/Socket/tcp_client.c
+++ Linux/Socket/tcp_client.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char const *argv[]) 
 { 
-	int sock = 0; 
+	int sock = 0, valread; 
 	struct sockaddr_in serv_addr; 
 	char *hello = "Hello from client"; 
 	char buffer[1024] = {0}; 
