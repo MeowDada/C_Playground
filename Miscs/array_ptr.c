02d3d0bc1f8c4842377ad4eca15b1845eb6783ea
MeowDada <chang.hungjui1251@gmail.com>
2019-10-14 09:24:53 +0800

    a pointer test

--- Miscs/array_ptr.c
+++ Miscs/array_ptr.c
@@ -9,6 +9,23 @@ int main(int argc, char **argv)
 	{
 		printf("%d ", i[a]);
 	}
+	printf("\n");
+
+	int b[10];
+	printf("int b[10];\n");
+	printf("mem address of b:      %p\n", b);
+	printf("mem address of (b+1):  %p\n", b+1);
+	printf("mem address of &b:     %p\n", &b);
+	printf("mem address of &(b+1): %p\n", &b+1);
+
+	printf("\n");
 	
+	int *c = malloc(sizeof(int)*10);
+	printf("int *c = malloc(sizeof(int)*10)\n");
+	printf("mem address of c:      %p\n", c);
+	printf("mem address of (c+1):  %p\n", c+1);
+	printf("mem address of &c:     %p\n", &c);
+	printf("mem address of &c+1:   %p\n", &c+1);
+
 	return 0;
 }
